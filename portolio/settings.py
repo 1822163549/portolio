4f669087fcd3edde67288e05c8925f676d5eded3
name <1822163549@qq.com>
2019-10-19 02:23:17 +0800

    添加了选择保存图片功能等重点在于数据库迁移，没迁移models导致一直使用以前的库用大量时间找问题
    在models里面添加文字记录功能description = models.CharField(max_length=100)              
                  description只是变量，名称看懂即可                   Field：领域，方面
    						image = models.ImageField
    						                                    Image：图片
    						
    使用image图形化界面需要安装pillow：pip install pillow
    
    给项目加标题(tilte)：
    class hdtjdh() ：
    	title = models.CharField(default='作品标题',max_length=50)
    	
    		def __str__(self):
    			return self.title
    			
    			
    当添加image想要查看时需要配置：
    在settings末尾：
    '''
    MEDIA_URL = '/media/'
    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
    '''
    在urls下导入：
    '''
    from django.conf.urls.static import static
    from django.conf import settings
    '''
    在urlpatterns后 ' ] ' 添加+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    这是一般格式，可更改的一般为网站地址和根目录（URL，ROOT）
    一但有models等框架数据更改就要进行数据库迁移
    (重点)改数据库model时一定要迁移否则还会是以前的数据库
    数据库迁移步骤①：python manage.py makemigrations
    		       ②：python manage.py migrate

--- portolio/settings.py
+++ portolio/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#图片网站
+MEDIA_URL = '/media/'
+
+#图片路径
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
